6ef18b9f4f26b41075c99e32cdaf69415fecda0c
Kory Attleson <kory.attleson@gmail.com>
2021-08-11 14:38:52 -0500

    Finailized battlefield.py and main.py to run through entire index of robots/dinos to battle

--- battlefield.py
+++ battlefield.py
@@ -8,55 +8,36 @@ class Battlefield:
         self.team_dinosaurs = Herd_of_dinosaurs()
         self.team_robots = Fleet_of_robots()
     
-    def round_one(self):
+    def battle_round(self, index):
         self.robots_attack = True
         self.dinosaurs_attack = True
         self.keep_fighting = True
         while self.keep_fighting:
             while self.dinosaurs_attack:
-                if(self.team_robots.robot_one.health > 0):
-                    self.team_robots.robot_one.health -= self.team_dinosaurs.dino_one.attack_damage
-                    print("Robot has " + str(self.team_robots.robot_one.health) + " left!")
-                    if(self.team_robots.robot_one.health == 0):
-                        print("Robot has fainted!")
+                if(self.team_robots.robot_fleet[index].health > 0):
+                    self.team_robots.robot_fleet[index].health -= self.team_dinosaurs.dino_herd[index].attack_damage
+                    print("Robot has " + str(self.team_robots.robot_fleet[index].health) + " health left!")
+                    if(self.team_robots.robot_fleet[index].health <= 0):
+                        print("Robot " + self.team_robots.robot_fleet[index].name + " has fainted!")
                         self.dinosaurs_attack = False
                         self.keep_fighting = False
                         self.robots_attack = False
                         break
                     if(self.dinosaurs_attack == True):
                         self.dinosaurs_attack = False
-                # elif(self.team_robots.robot_one.health == 0):
-                #     print("Robot has fainted!")
-                #     self.dinosaurs_attack = False
-                #     self.keep_fighting = False
             while self.robots_attack:
-                if(self.team_dinosaurs.dino_one.health > 0):
-                    self.team_dinosaurs.dino_one.health -= self.team_robots.robot_one.attack_damage
-                    print("Dinosaur has " + str(self.team_dinosaurs.dino_one.health) + " left!")
+                if(self.team_dinosaurs.dino_herd[index].health > 0):
+                    self.team_dinosaurs.dino_herd[index].health -= self.team_robots.robot_fleet[index].attack_damage
+                    print("Dinosaur has " + str(self.team_dinosaurs.dino_herd[index].health) + " health left!")
+                    if(self.team_dinosaurs.dino_herd[index].health <= 0):
+                        print("Dinosaur " + self.team_dinosaurs.dino_herd[index].name + " has fainted!")
+                        self.dinosaurs_attack = False
+                        self.keep_fighting = False
+                        self.robots_attack = False
+                        break
                     if(self.robots_attack == True):
                         self.robots_attack = False
-                    else:
-                        self.robots_attack = True
-                elif(self.team_dinosaurs.dino_one.health == 0):
-                    print("Dinosaur has fainted!")
-                    self.robots_attack = False
             if(self.dinosaurs_attack == False and self.keep_fighting == True):
                     self.dinosaurs_attack = True
             if(self.robots_attack == False and self.keep_fighting == True):
                 self.robots_attack = True
-
-    def next_turn(self):
-        if (self.dinosaurs_attack == True):
-            self.dinosaurs_attack = False
-        else:
-            self.dinosaurs_attack = True
-    
-    def next_turn1(self):
-        if (self.robots_attack == True):
-            self.robots_attack = False
-        else:
-            self.robots_attack = True
-
-    # def round_two(self, robots, dinosaurs):
-
-    # def round_three(self, robots, dinosaurs):
